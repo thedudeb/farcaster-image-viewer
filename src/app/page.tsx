214f29a32869efac1a9eca159237aa446b4a25f7
thedudeb <thedudea7118@gmail.com>
2025-08-12 12:37:27 -0300

    Update Epoch 5 disclaimer message to say 'created and curated'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
       const timer = setTimeout(() => {
         window.dispatchEvent(new CustomEvent('showNotification', { 
           detail: { 
-            message: `This one was curated, and created by another amazing artist, @Greywash`,
+            message: `This one was created and curated by another amazing artist, @Greywash`,
             type: 'epoch5-notice',
             artistProfile: 'https://warpcast.com/greywash'
           } 
